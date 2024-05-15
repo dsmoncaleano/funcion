54ea50132bd40913f9e7258422c299f0a45ff65a
dsmoncaleano <168672204+dsmoncaleano@users.noreply.github.com>
2024-05-14 19:37:08 -0500

    Update 8 (1).py

--- python/8 (1).py
+++ python/8 (1).py
@@ -1,13 +1,5 @@
 def contar_mayusculas_minusculas(cadena):
-    """
-    Calcula el número de letras mayúsculas y minúsculas en una cadena.
-    
-    Parámetros:
-    cadena (str): La cadena a analizar.
-    
-    Retorna:
-    dict: Un diccionario con el conteo de letras mayúsculas y minúsculas.
-    """
+  
     conteo = {'mayusculas': 0, 'minusculas': 0}
     
     for caracter in cadena:
@@ -18,8 +10,9 @@ def contar_mayusculas_minusculas(cadena):
     
     return conteo
 
-# Ejemplo de uso:
+
 cadena = "Hola Mundo! Esto Es Una Prueba."
 resultado = contar_mayusculas_minusculas(cadena)
 print(f"Letras mayúsculas: {resultado['mayusculas']}")
 print(f"Letras minúsculas: {resultado['minusculas']}")
+
